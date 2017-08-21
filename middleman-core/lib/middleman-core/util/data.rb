c45bfc9e06546b562c5c485741ef3068bc4acfff
Cristian Bica <cristian.bica@gmail.com>
2017-08-21 18:04:05 +0200

    Update data.rb

--- middleman-core/lib/middleman-core/util/data.rb
+++ middleman-core/lib/middleman-core/util/data.rb
@@ -48,6 +48,7 @@ module Middleman
       Contract IsA['Middleman::SourceFile'], Maybe[Symbol] => [Hash, Maybe[String]]
       def parse(file, frontmatter_delims, known_type=nil)
         full_path = file[:full_path]
+        puts "Parsing #{full_path} ..."
         return [{}, nil] if ::Middleman::Util.binary?(full_path) || file[:types].include?(:binary)
 
         # Avoid weird race condition when a file is renamed
